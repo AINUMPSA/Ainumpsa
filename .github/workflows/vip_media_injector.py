faffb20333e4fc5d6682331e18800c80ebe0f259
Numpsa <numpsa@gmail.com>
2026-07-29 03:19:12 +0100

    Delete .github/workflows directory

--- .github/workflows/vip_media_injector.py
+++
@@ -1,5 +0,0 @@
-      - name: Run Hyper Version 2.14 Engine & VIP Media Injector
-        run: |
-          mkdir -p inputs/media Knowledge_base
-          python vip_media_injector.py
-          python hyper_version_2_14.py
